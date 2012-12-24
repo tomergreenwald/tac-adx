cd7949710c52265801d14a10370936fdf67ca7f8
tomerg@gmail.com <tomerg@gmail.com>
2012-12-24 10:33:56 +0200

    Added server folder.
    Former-commit-id: 7d1d38c530ae3948bdf51e1e80a72e3aa92e6735 [formerly c3fe9e9fc3a1e78673af0f6d862f4b936dccfe01]
    Former-commit-id: 87c76ad6a79a95639fa1a8239e00215b078f97dd

---
+++ AdX/aa-server-10.1.0.0/runServer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Usage
+#   sh ./runServer.sh
+#
+
+TACAA_HOME=`pwd`
+LIB=${TACAA_HOME}/lib
+CLASSPATH=.
+for i in $( ls ${LIB}/*.jar ); do
+    CLASSPATH=${CLASSPATH}:$i
+done
+echo $TACAA_HOME
+echo $CLASSPATH
+
+#java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=y -cp $CLASSPATH se.sics.tasim.sim.Main
+java -cp $CLASSPATH se.sics.tasim.sim.Main
