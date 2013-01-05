f3384b475ee9a1e0c0dbedff7efef23149d3998a
tomerg@gmail.com <tomerg@gmail.com>
2013-01-05 12:21:34 +0200

    Added AdxInfoContextFactoryTest.
    Former-commit-id: 4e89ec6977c2e8486d325df67585fe812dda71ce [formerly 277c0d6616e2d6fb1028fd132350358394e7ef3b]
    Former-commit-id: 3824dadcf21dd0b4a054ca00ce3ad52db7e0c995

---
+++ AdX/src/test/java/tau/tac/adx/props/AdxInfoContextFactoryTest.java
@@ -0,0 +1,66 @@
+/*
+ * AdxInfoContextFactoryTest.java
+ *
+ * COPYRIGHT  2008
+ * THE REGENTS OF THE UNIVERSITY OF MICHIGAN
+ * ALL RIGHTS RESERVED
+ *
+ * PERMISSION IS GRANTED TO USE, COPY, CREATE DERIVATIVE WORKS AND REDISTRIBUTE THIS
+ * SOFTWARE AND SUCH DERIVATIVE WORKS FOR NONCOMMERCIAL EDUCATION AND RESEARCH
+ * PURPOSES, SO LONG AS NO FEE IS CHARGED, AND SO LONG AS THE COPYRIGHT NOTICE
+ * ABOVE, THIS GRANT OF PERMISSION, AND THE DISCLAIMER BELOW APPEAR IN ALL COPIES
+ * MADE; AND SO LONG AS THE NAME OF THE UNIVERSITY OF MICHIGAN IS NOT USED IN ANY
+ * ADVERTISING OR PUBLICITY PERTAINING TO THE USE OR DISTRIBUTION OF THIS SOFTWARE
+ * WITHOUT SPECIFIC, WRITTEN PRIOR AUTHORIZATION.
+ *
+ * THIS SOFTWARE IS PROVIDED AS IS, WITHOUT REPRESENTATION FROM THE UNIVERSITY OF
+ * MICHIGAN AS TO ITS FITNESS FOR ANY PURPOSE, AND WITHOUT WARRANTY BY THE
+ * UNIVERSITY OF MICHIGAN OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+ * LIMITATION THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE REGENTS OF THE UNIVERSITY OF MICHIGAN SHALL NOT BE LIABLE FOR ANY
+ * DAMAGES, INCLUDING SPECIAL, INDIRECT, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, WITH
+ * RESPECT TO ANY CLAIM ARISING OUT OF OR IN CONNECTION WITH THE USE OF THE SOFTWARE,
+ * EVEN IF IT HAS BEEN OR IS HEREAFTER ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ */
+package tau.tac.adx.props;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+
+import org.junit.Test;
+
+import se.sics.isl.transport.Context;
+
+/**
+ * Unit test suite for the AdxInfoContextFactory class.
+ * 
+ * @author Patrick Jordan
+ */
+public class AdxInfoContextFactoryTest {
+
+	@Test
+	public void testConstructor() {
+		new AdxInfoContextFactory();
+	}
+
+	@Test
+	public void testEmptyCreateContext() {
+		AdxInfoContextFactory info = new AdxInfoContextFactory();
+		assertNotNull(info.createContext());
+	}
+
+	@Test
+	public void testParentCreateContext() {
+		AdxInfoContextFactory info = new AdxInfoContextFactory();
+		Context context = new Context("parent");
+
+		Context childContext = info.createContext(context);
+		assertNotNull(childContext);
+
+		/*
+		 * The same object should be returned if the last context already exists
+		 * with the same parent.
+		 */
+		assertSame(childContext, info.createContext(context));
+	}
+}
