440574aeebc90007847c31a74b9c3821f7d1e42b
tomerg@gmail.com <tomerg@gmail.com>
2013-04-19 19:46:13 +0300

    Removed unused agent startets
    Former-commit-id: 819f61be77d54228903354ba7eb86148155b9c21 [formerly e2de1ccd1c57f60558c662c962f58f71a103dd64]
    Former-commit-id: bba049f2352c24b9cf0075b8115c00941d996708

--- AdX/adx-agnet-1.0.0.0/runAgent.sh
+++ AdX/adx-agnet-1.0.0.0/runAgent.sh
@@ -8,11 +8,4 @@ TACAA_HOME=`pwd`
 echo $TACAA_HOME
 echo $CLASSPATH
 
-java -cp "lib/*" tau.tac.adx.agentware.Main -config config/aw-1.conf&
-java -cp "lib/*" tau.tac.adx.agentware.Main -config config/aw-2.conf&
-java -cp "lib/*" tau.tac.adx.agentware.Main -config config/aw-3.conf&
-java -cp "lib/*" tau.tac.adx.agentware.Main -config config/aw-4.conf&
-java -cp "lib/*" tau.tac.adx.agentware.Main -config config/aw-5.conf&
-java -cp "lib/*" tau.tac.adx.agentware.Main -config config/aw-6.conf&
-java -cp "lib/*" tau.tac.adx.agentware.Main -config config/aw-7.conf&
-java -cp "lib/*" tau.tac.adx.agentware.Main -config config/aw-8.conf&
+java -cp "lib/*" tau.tac.adx.agentware.Main -config config/aw-1.conf
