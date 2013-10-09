ca5485f2cd7dcafc313848aebbb7017dc5736bb6
tomerg@gmail.com <tomerg@gmail.com>
2013-10-09 21:25:14 +0200

    Added publisher catalog parser
    Former-commit-id: 6a1c2cff8c9f435d5a472fa604313dc0e577665b [formerly 9aa57cd60fab95d0e97fb584787f1e3aca14b4ea]
    Former-commit-id: fb78b15eb6c1cb1d741fcdfd5bbd3b182ac67880

---
+++ AdX/src/main/java/edu/umich/eecs/tac/parser/bankstatus/PublisherCatalogParser.java
@@ -0,0 +1,50 @@
+package edu.umich.eecs.tac.parser.bankstatus;
+
+import java.util.List;
+
+import se.sics.isl.transport.Transportable;
+import se.sics.tasim.logtool.LogReader;
+import tau.tac.adx.props.PublisherCatalog;
+import tau.tac.adx.props.PublisherCatalogEntry;
+import edu.umich.eecs.tac.Parser;
+
+/**
+ * <code>BankStatusParser</code> is a simple example of a TAC AA parser that
+ * prints out all advertiser's BankStatus received in a simulation from the
+ * simulation log file.
+ * <p>
+ * <p/>
+ * The class <code>Parser</code> is inherited to provide base functionality for
+ * TAC AA log processing.
+ * 
+ * @author - Lee Callender
+ * 
+ * @see edu.umich.eecs.tac.Parser
+ */
+public class PublisherCatalogParser extends Parser {
+
+	boolean first = true;
+
+	public PublisherCatalogParser(LogReader reader) {
+		super(reader);
+		System.out.println("Participating publishers: \n");
+	}
+
+	@Override
+	protected void dataUpdated(int type, Transportable content) {
+
+	}
+
+	@Override
+	protected void message(int sender, int receiver, Transportable content) {
+		if (first && content instanceof PublisherCatalog) {
+			PublisherCatalog publisherCatalog = (PublisherCatalog) content;
+			List<PublisherCatalogEntry> publishers = publisherCatalog
+					.getPublishers();
+			for (PublisherCatalogEntry publisherCatalogEntry : publishers) {
+				System.out.println(publisherCatalogEntry.getPublisherName());
+			}
+			first = false;
+		}
+	}
+}
