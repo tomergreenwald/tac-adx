2ace59340f607408bc68b34ce7a37fe278884b8f
tomerg@gmail.com <tomerg@gmail.com>
2013-10-12 10:59:51 +0200

    Added ucs parser
    Former-commit-id: 514ffc0c697a98392c3a0db698cee3f2f3742e0c [formerly b20057ef06c383d96893b9388e3ce220ed628a24]
    Former-commit-id: 22e5adaf59b48dcb1d812fa5117b08ad77c6dc25

---
+++ AdX/src/main/java/edu/umich/eecs/tac/parser/bankstatus/UCSParser.java
@@ -0,0 +1,73 @@
+package edu.umich.eecs.tac.parser.bankstatus;
+
+import org.apache.commons.lang3.StringUtils;
+
+import se.sics.isl.transport.Transportable;
+import se.sics.tasim.logtool.LogReader;
+import se.sics.tasim.logtool.ParticipantInfo;
+import tau.tac.adx.report.demand.AdNetworkDailyNotification;
+import tau.tac.adx.sim.TACAdxConstants;
+import edu.umich.eecs.tac.Parser;
+
+/**
+ * <code>BankStatusParser</code> is a simple example of a TAC AA parser that
+ * prints out all advertiser's BankStatus received in a simulation from the
+ * simulation log file.
+ * <p>
+ * <p/>
+ * The class <code>Parser</code> is inherited to provide base functionality for
+ * TAC AA log processing.
+ * 
+ * @author - Lee Callender
+ * 
+ * @see edu.umich.eecs.tac.Parser
+ */
+public class UCSParser extends Parser {
+
+	private final int day = 0;
+	private final String[] participantNames;
+	private final boolean[] is_Advertiser;
+	private final ParticipantInfo[] participants;
+
+	public UCSParser(LogReader reader) {
+		super(reader);
+
+		// Print agent indexes/gather names
+		System.out.println("****AGENT INDEXES****");
+		participants = reader.getParticipants();
+		if (participants == null) {
+			throw new IllegalStateException("no participants");
+		}
+		int agent;
+		participantNames = new String[participants.length];
+		is_Advertiser = new boolean[participants.length];
+		for (int i = 0, n = participants.length; i < n; i++) {
+			ParticipantInfo info = participants[i];
+			agent = info.getIndex();
+			System.out.println(info.getName() + ": " + agent);
+			participantNames[agent] = info.getName();
+			if (info.getRole() == TACAdxConstants.ADVERTISER) {
+				is_Advertiser[agent] = true;
+			} else
+				is_Advertiser[agent] = false;
+		}
+
+		System.out.println("****User classification***");
+		System.out.println(StringUtils.rightPad("Agent", 20)
+				+ "\tDay\tBank Status");
+	}
+
+	@Override
+	protected void dataUpdated(int type, Transportable content) {
+
+	}
+
+	@Override
+	protected void message(int sender, int receiver, Transportable content) {
+		if (content instanceof AdNetworkDailyNotification) {
+			AdNetworkDailyNotification dailyNotification = (AdNetworkDailyNotification) content;
+			System.out.println(StringUtils.rightPad(participantNames[receiver],
+					20) + "\t" + dailyNotification.getServiceLevel());
+		}
+	}
+}
