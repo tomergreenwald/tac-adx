38bcd112e869dbbccf6fc0aa6d2a9c2ba105987d
Tomer <tomerg@gmail.com>
2014-02-07 21:06:18 +0200

    Added support for campaign ID
    Former-commit-id: 398665be1836acd13815d4e59aa932989400ff2a [formerly 7a748b017b2c76c9eaa74f5f82b3d8165399846b]
    Former-commit-id: 1cae275eae7ca54b8800a4768446e432a6dd1f46

--- AdX/src/main/java/tau/tac/adx/report/demand/campaign/auction/CampaignAuctionReport.java
+++ AdX/src/main/java/tau/tac/adx/report/demand/campaign/auction/CampaignAuctionReport.java
@@ -1,10 +1,14 @@
 package tau.tac.adx.report.demand.campaign.auction;
 
+import java.text.ParseException;
+
+import se.sics.isl.transport.TransportReader;
+import se.sics.isl.transport.TransportWriter;
 import edu.umich.eecs.tac.props.AbstractKeyedEntryList;
 
 /**
  * 
- * @author Mariano Schain
+ * @author Tomer Greenwald
  * 
  */
 public class CampaignAuctionReport extends
@@ -14,6 +18,27 @@ public class CampaignAuctionReport extends
 	 * 
 	 */
 	private static final long serialVersionUID = -383908225939942653L;
+	
+	/** Campaign id key. */
+	private static String CAMPAIGN_ID_KEY = "CAMPAIGN_ID_KEY"; 
+	
+	/** Campaign ID. */
+	private int campaignID;
+
+	/**
+	 * Default constructor.
+	 */
+	public CampaignAuctionReport() {
+		super();
+	}
+
+	/**
+	 * @param campaignID
+	 */
+	public CampaignAuctionReport(int campaignID) {
+		super();
+		this.campaignID = campaignID;
+	}
 
 	@Override
 	protected CampaignAuctionReportEntry createEntry(CampaignAuctionReportKey key) {
@@ -61,6 +86,17 @@ public class CampaignAuctionReport extends
 	public CampaignAuctionReportEntry getCampaignReportEntry(
 			CampaignAuctionReportKey campaignReportKey) {
 		return getEntry(campaignReportKey);
+	}
+	
+	@Override
+	protected void readBeforeEntries(TransportReader reader)
+			throws ParseException {
+		campaignID = reader.getAttributeAsInt(CAMPAIGN_ID_KEY);
+	}
+	
+	@Override
+	protected void writeBeforeEntries(TransportWriter writer) {
+		writer.attr(CAMPAIGN_ID_KEY, campaignID);
 	}
 
 }
