e5cd0b77f335961b4796770317aa0c95cb0fd855
Tomer <tomerg@gmail.com>
2014-06-07 17:12:24 +0300

    Added the SystemTestMessageValidation class. Added a test for the beginning day of CampaignOpportunity messages
    Former-commit-id: 54afa6f96cb48fe6b8c7050b34d2dc8f680f1124 [formerly 9371f317add4609fc080776676581579205de325]
    Former-commit-id: a7046323aa5f61ed4783313b054d44c3fba7697e

---
+++ AdX/src/test/java/tau/tac/adx/system/SystemTestMessageValidation.java
@@ -0,0 +1,101 @@
+/*
+ * SalesReportTest.java
+ *
+ * COPYRIGHT  2008
+ * THE REGENTS OF THE UNIVERSITY OF MICHIGAN
+ * ALL RIGHTS RESERVED
+ *
+ * PERMISSION IS GRANTED TO USE, COPY, CREATE DERIVATIVE WORKS AND REDISTRIBUTE THIS
+ * SOFTWARE AND SUCH DERIVATIVE WORKS FOR NONCOMMERCIAL EDUCATION AND RESEARCH
+ * PURPOSES, SO LONG AS NO FEE IS CHARGED, AND SO LONG AS THE COPYRIGHT NOTICE
+ * ABOVE, THIS GRANT OF PERMISSION, AND THE DISCLAIMER BELOW APPEAR IN ALL COPIES
+ * MADE; AND SO LONG AS THE NAME OF THE UNIVERSITY OF MICHIGAN IS NOT USED IN ANY
+ * ADVERTISING OR PUBLICITY PERTAINING TO THE USE OR DISTRIBUTION OF THIS SOFTWARE
+ * WITHOUT SPECIFIC, WRITTEN PRIOR AUTHORIZATION.
+ *
+ * THIS SOFTWARE IS PROVIDED AS IS, WITHOUT REPRESENTATION FROM THE UNIVERSITY OF
+ * MICHIGAN AS TO ITS FITNESS FOR ANY PURPOSE, AND WITHOUT WARRANTY BY THE
+ * UNIVERSITY OF MICHIGAN OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT
+ * LIMITATION THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE REGENTS OF THE UNIVERSITY OF MICHIGAN SHALL NOT BE LIABLE FOR ANY
+ * DAMAGES, INCLUDING SPECIAL, INDIRECT, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, WITH
+ * RESPECT TO ANY CLAIM ARISING OUT OF OR IN CONNECTION WITH THE USE OF THE SOFTWARE,
+ * EVEN IF IT HAS BEEN OR IS HEREAFTER ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ */
+package tau.tac.adx.system;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import se.sics.tasim.logtool.LogReader;
+import tau.tac.adx.parser.LogVerifierParser;
+import tau.tac.adx.report.demand.CampaignOpportunityMessage;
+
+/**
+ * @author Tomer Greenwald
+ * @author Mariano Schain
+ */
+@RunWith(Parameterized.class)
+public class SystemTestMessageValidation {
+
+	/**
+	 * {@link LogVerifierParser} instance to be used for verification.
+	 */
+	private static LogVerifierParser parser;
+
+	/**
+	 * Day number for the parameterized tests.
+	 */
+	private int day;
+
+	public SystemTestMessageValidation(int day) {
+		this.day = day;
+	}
+
+	@Parameters(name = "day #{0}")
+	public static Iterable<Object[]> data() throws FileNotFoundException,
+			IOException, ParseException {
+		LogReader logReader = new LogReader(new FileInputStream(
+				"src\\test\\resources\\localhost_sim399.slg"));
+		parser = new LogVerifierParser(logReader, null);
+		parser.start();
+		parser.stop();
+		List<Object[]> data = new LinkedList<Object[]>();
+		for (int verifiedDay = 0; verifiedDay <= 60; verifiedDay++) {
+			data.add(new Object[] { verifiedDay });
+		}
+		return data;
+	}
+
+	@Test
+	public void testCampaignOpportunityStartDay() {
+		CampaignOpportunityMessage campaignOpportunityMessage = parser.campaignOpportunityMessages
+				.get(day);
+		Assume.assumeTrue("No campaign was allocated today", campaignOpportunityMessage != null);
+		String message = campaignOpportunityStartDayMessage(campaignOpportunityMessage);
+		Assert.assertEquals(message, day + 2,
+				campaignOpportunityMessage.getDayStart());
+	}
+
+	private String campaignOpportunityStartDayMessage(
+			CampaignOpportunityMessage campaignOpportunityMessage) {
+		return "Expected campaign #"
+				+ campaignOpportunityMessage.getId()
+				+ " to begin at day #"
+				+ (day + 2)
+				+ " (two days after the campaign oppotunity message arrived) but it actually starts at day #"
+				+ campaignOpportunityMessage.getDayStart();
+	}
+
+}
