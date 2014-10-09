24abca9beec94e8c4520cb5e03299707c5fb8710
Tomer Greenwald <tomerg@gmail.com>
2014-10-09 09:32:06 +0300

    More efficient code
    Former-commit-id: 365db69c7a83c8d698065db06c3f1c1f57424bba [formerly 5e90db2d7609e8d87626d7e75338d41852afd5d8]
    Former-commit-id: 60136fbfedf04ff14a58ba2f2ff99acbd87f4fdf

--- AdX/src/main/cpp/reserve-price-payground/reserve-price-payground/reserve-price-payground.cpp
+++ AdX/src/main/cpp/reserve-price-payground/reserve-price-payground/reserve-price-payground.cpp
@@ -59,26 +59,24 @@ bool end_point_sorter(EndPoint const& lhs, EndPoint const& rhs) {
 
 PointData get_boundary_points(VFunction* functions, uint64_t length) {
 	PointData	res;
-	std::vector<EndPoint> points(length * 3);
-	double		sum = 0;
+
+	res.points.resize(length * 3);
 
 	for (int j = 0; j < length; j++) {
-		sum += functions[j].points.a1;
+		res.sum += functions[j].points.a1;
 
-		points[j * 3].val = functions[j].boundary.high;
-		points[j * 3].point_type = FunctionType::HIGH;
-		points[j * 3].function = functions[j];
+		res.points[j * 3].val = functions[j].boundary.high;
+		res.points[j * 3].point_type = FunctionType::HIGH;
+		res.points[j * 3].function = functions[j];
 
-		points[j * 3 + 1].val = functions[j].boundary.low;
-		points[j * 3 + 1].point_type = FunctionType::LOW;
-		points[j * 3 + 1].function = functions[j];
+		res.points[j * 3 + 1].val = functions[j].boundary.low;
+		res.points[j * 3 + 1].point_type = FunctionType::LOW;
+		res.points[j * 3 + 1].function = functions[j];
 
-		points[j * 3 + 2].val = functions[j].boundary.high * (1 + MICRO_S);
-		points[j * 3 + 2].point_type = FunctionType::MICRO;
-		points[j * 3 + 2].function = functions[j];
+		res.points[j * 3 + 2].val = functions[j].boundary.high * (1 + MICRO_S);
+		res.points[j * 3 + 2].point_type = FunctionType::MICRO;
+		res.points[j * 3 + 2].function = functions[j];
 	}
-	res.points = points;
-	res.sum = sum;
 	return res;
 }
 
