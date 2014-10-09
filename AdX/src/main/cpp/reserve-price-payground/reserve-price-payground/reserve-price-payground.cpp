0ec98a091dba8a433efbb8cd36b4185e6ba6dd9d
Tomer Greenwald <tomerg@gmail.com>
2014-10-09 10:23:51 +0300

    Fixed logic
    Former-commit-id: 9d2a1be72c8b7422aa0515c9f8f37c36a4312b15 [formerly d95e2ca65e60ba13f369049a31ee29b55ea997c0]
    Former-commit-id: 1d4528da11057e61e5d22ea73ec9020c2b0187df

--- AdX/src/main/cpp/reserve-price-payground/reserve-price-payground/reserve-price-payground.cpp
+++ AdX/src/main/cpp/reserve-price-payground/reserve-price-payground/reserve-price-payground.cpp
@@ -16,17 +16,18 @@ bool end_point_sorter(EndPoint const& lhs, EndPoint const& rhs) {
 PointData get_boundary_points(VFunction* functions, uint64_t length) {
 	PointData	res;
 
+	res.sum = 0;
 	res.points.resize(length * 3);
 
 	for (int j = 0; j < length; j++) {
 		res.sum += functions[j].points.a1;
 
-		res.points[j * 3].val = functions[j].boundary.high;
-		res.points[j * 3].point_type = FunctionType::HIGH;
+		res.points[j * 3].val = functions[j].boundary.low;
+		res.points[j * 3].point_type = FunctionType::LOW;
 		res.points[j * 3].function = functions[j];
 
-		res.points[j * 3 + 1].val = functions[j].boundary.low;
-		res.points[j * 3 + 1].point_type = FunctionType::LOW;
+		res.points[j * 3 + 1].val = functions[j].boundary.high;
+		res.points[j * 3 + 1].point_type = FunctionType::HIGH;
 		res.points[j * 3 + 1].function = functions[j];
 
 		res.points[j * 3 + 2].val = functions[j].boundary.high * (1 + MICRO_S);
@@ -86,6 +87,7 @@ double calculate_stuff(PointData pointData, uint64_t length) {
 				best_score = currentC.s;
 				best_reserve = pointData.points[j].val;
 			}
+			previousC = currentC;
 		}
 	}
 	return best_reserve;
