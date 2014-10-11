310c80bdd0c62518277d91f1c4c9d41006b185b8
Tomer Greenwald <tomerg@gmail.com>
2014-10-11 17:57:17 +0300

    Fixed spaces
    Former-commit-id: 37947c089ed655fa9d12964e68b70c87793c8bcb [formerly 4af6722ed65007734eae17b3afd653427a0762f3]
    Former-commit-id: 2bef891ef3299279917d1f06dc6ab7b484a93437

--- AdX/src/main/cpp/reserve-price-payground/reserve-price-payground/DataStructs.h
+++ AdX/src/main/cpp/reserve-price-payground/reserve-price-payground/DataStructs.h
@@ -2,41 +2,41 @@
 #include <vector>
 
 namespace tau {
-	namespace tac {
+namespace tac {
 
-		static const double MICRO_S = 0.01;
+static const double MICRO_S = 0.01;
 
-		struct Boundary {
-			double low, high;
-		};
+struct Boundary {
+	double low, high;
+};
 
-		struct C {
-			double c1, c2, c3, c4, s, point;
-		};
+struct C {
+	double c1, c2, c3, c4, s, point;
+};
 
-		enum FunctionType {
-			LOW, HIGH, MICRO
-		};
+enum FunctionType {
+	LOW, HIGH, MICRO
+};
 
-		struct Points {
-			double a1, a2, a3, a4;
-		};
+struct Points {
+	double a1, a2, a3, a4;
+};
 
-		struct VFunction {
-			Boundary boundary;
-			Points points;
-		};
+struct VFunction {
+	Boundary boundary;
+	Points points;
+};
 
-		struct EndPoint {
-			double val;
-			FunctionType point_type;
-			VFunction function;
-		};
+struct EndPoint {
+	double val;
+	FunctionType point_type;
+	VFunction function;
+};
 
-		struct PointData {
-			std::vector<EndPoint> points;
-			double sum;
-		};
+struct PointData {
+	std::vector<EndPoint> points;
+	double sum;
+};
 
-	} //namespace tac
+} //namespace tac
 } //namespace tau
